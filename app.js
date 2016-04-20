19fa3a52732601ad8ea59ece240a5ea767590f8a
Boom <boom@Booms-MacBook-Pro.local>
2016-04-20 23:17:14 +0700

    edit

--- app.js
+++ app.js
@@ -4,18 +4,22 @@ angular.module('randomApp', [])
     $scope.arr = []
     $scope.bingo = []
 
-      while($scope.arr.length < 99){
-        var randomnumber=Math.ceil(Math.random()*99)
-        var found=false;
-        for(var i=0;i<$scope.arr.length;i++){
-        if($scope.arr[i]==randomnumber){found=true;break}
-        }
-        if(!found){
-          $scope.arr[$scope.arr.length]=randomnumber
+    while ($scope.arr.length < 99) {
+      var randomnumber = Math.ceil(Math.random() * 99)
+      var found = false
+      for (var i = 0;i < $scope.arr.length;i++) {
+        if ($scope.arr[i] === randomnumber) {
+          found = true
+          break
         }
       }
+      if (!found) {
+        $scope.arr[$scope.arr.length] = randomnumber
+      }
+    }
 
     $scope.play = function () {
       $scope.bingo.push($scope.arr.splice(0, 1))
+      $scope.show = $scope.bingo.map(function (obj) { return obj[0] })
     }
   })
